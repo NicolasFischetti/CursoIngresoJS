0d8623010e8492dc6df774cb1b45e0d25e0fc76e
NicolasFischetti <36281576+NicolasFischetti@users.noreply.github.com>
2018-02-09 22:13:38 -0300

    Ejercicio 4 y 5 if

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,14 @@
 function Mostrar()
 {
-//tomo la edad  
+//tomo a edad  
+	var edad;
+	edad= document.getElementById('edad').value;
 
+	if (edad < 13 || edad > 17) 
+	{
+
+		alert("no es adolescente");
+		
+	}
 
 }//FIN DE LA FUNCIÓN
