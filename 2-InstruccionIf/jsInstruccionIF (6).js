e2d569113180b4a1e314577343a57e602cb51859
NicolasFischetti <36281576+NicolasFischetti@users.noreply.github.com>
2018-02-13 18:28:40 -0300

    Ejercicio 6

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -2,6 +2,39 @@ function Mostrar()
 {
 //tomo la edad  
 
+	var edad;
+	edad= document.getElementById('edad').value;
+
+	if (edad >= 18)
+
+		{	
+
+				alert("La persona es mayor");
+
+
+		}
+		
+
+
+		if (edad >= 13 && edad <= 17) 
+
+		{
+
+			aler("La persona es adolescente ");
+
+
+		}
+
+		else
+
+			{
+				alert("La persona es menor");
+
+
+				}
+
+
+		
 
 
 
