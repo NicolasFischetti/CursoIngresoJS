6b3911372aca0f6fbd66c692b5f2fa0fbaafb858
NicolasFischetti <36281576+NicolasFischetti@users.noreply.github.com>
2018-02-14 13:26:31 -0300

    Ejercicio 7

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -2,7 +2,35 @@ function Mostrar()
 {
 //tomo la edad  
 
-	
+	var edad;
+	var soltero;
+	var divorciado;
+	var casado;
 
+	edad= document.getElementById('edad').value;
+	casado= document.getElementById("estadoCivil").value;
+	divorciado= document.getElementById("estadoCivil").value;
+	soltero= document.getElementById("estadoCivil").value;
+
+
+	if (edad < 18 !== "casado")
+
+	{
+		alert("Es muy pequeño para NO ser soltero");
+
+	}
+
+	else if (edad < 18 !== "divorciado")
+
+	{
+
+		alert("Es muy pequeño para NO ser soltero");
+	}
+
+	else 
+
+	{
+		alert("Es muy pequeño NO para ser soltero");
+	}
 
 }//FIN DE LA FUNCIÓN
