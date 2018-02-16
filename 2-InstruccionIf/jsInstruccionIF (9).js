a51c49c4c64b0fb891a72173437a2dc9d3135e4c
NicolasFischetti <36281576+NicolasFischetti@users.noreply.github.com>
2018-02-15 22:09:16 -0300

    Ejercicio 9
    random

--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,21 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var numerorandom;
+
+	numerorandom= Math.floor(Math.random() * 10) + 1;
+
+
+
+	alert("El numero random es: "+numerorandom);
+
+
+	
+
+
+	
+	
+
 	
 
 }//FIN DE LA FUNCIÓN
