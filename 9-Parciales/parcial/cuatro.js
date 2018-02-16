bb3a66eb2235b44afeb0bdd71c569dbe8b042b4e
NicolasFischetti <36281576+NicolasFischetti@users.noreply.github.com>
2018-02-16 20:42:03 -0300

    Ejercicio 4

--- 9-Parciales/parcial/cuatro.js
+++ 9-Parciales/parcial/cuatro.js
@@ -1,4 +1,41 @@
 function Mostrar()
 {
 
+	var numero_1;
+	var numero_2;
+	var resultado;
+
+	numero_1= prompt("Numero uno");
+	numero_2= prompt("Numero dos");
+
+	numero_1= parseInt(numero_1);
+	numero_2= parseInt(numero_2);
+
+	if (numero_1 == numero_2)
+
+	{
+		resultado= numero_1 * numero_2;
+	}
+
+	else 
+
+	{
+
+	}
+
+	if (numero_1 > numero_2)
+
+	{
+		resultado= numero_1 - numero_2;
+
+	}
+
+	else
+	{
+		resultado= numero_1 + numero_2;
+		
+	}
+
+	document.write(resultado);
+
 }
