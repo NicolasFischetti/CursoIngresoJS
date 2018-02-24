433130ef8e916c9943fea25b74f411182faa36ef
NicolasFischetti <36281576+NicolasFischetti@users.noreply.github.com>
2018-02-24 15:50:51 -0300

    Ejercicio 7 aclaraciones

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -4,15 +4,19 @@ function Mostrar()
 	var contador=0;
 	var acumulador=0;
 	var respuesta='si';
+	var numero;
+	//var confirm= true;
 
-	while (respuesta== "si")
+	while (respuesta== "si")//puede ser true en vez de si
 	{
 		contador= contador+1;
 		numero= prompt("ingresar numero");
 		numero= parseInt(numero);
 		acumulador= acumulador+numero;
 		respuesta= prompt("para continuar si");
+		// valido siempre con while puedo tomar el numero por prompt y despues por parseint
 	}
+	// respuesta=confirm ("Desea ingresar otro numero? si/no")
 
 
 document.getElementById('suma').value=acumulador;
