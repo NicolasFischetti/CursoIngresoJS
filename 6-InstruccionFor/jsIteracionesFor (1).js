94b30e0235b23fc360248f6bad3604c0d0c33c4a
NicolasFischetti <36281576+NicolasFischetti@users.noreply.github.com>
2018-02-23 22:10:30 -0300

    For 1

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,4 +1,13 @@
 function Mostrar()
 {
+	// en for se pone la variable dentro de los parentesis sino toma lo de afuera tambien
+	   //unica vez; segunda; tercera. Se puede usar break. Puede haber mas de una variable
+		// for (;;) no uso las tres variables, bucle infinito
 
+		
+
+	for (var cant=1; cant<11;cant++)
+	{
+		console.log(cant);
+	}
 }
