e06cdf7e654d3cbf1ace0ac65ad6ccb534b6e582
NicolasFischetti <36281576+NicolasFischetti@users.noreply.github.com>
2018-02-27 16:14:55 -0300

    5 , 6 y 7

--- 9-Parciales/parcial/seis.js
+++ 9-Parciales/parcial/seis.js
@@ -1,4 +1,62 @@
 function Mostrar()
 {
+	//Realizar el algoritmo que pida el importe de las ventas (validar que sea mayor a 0,”cero”) de los 24 días
+	//hábiles del mes por prompt (una por día), e informar cual fue el mayor importe y cuál fue el menor
+	//importe de venta.
+
+
+	
+	var contador= 0;
+	var acumulador= 0;
+	var importe;
+	var min;
+	var max;
+	
+	
+
+	while (contador <= 23)
+
+	{
+		contador++;
+		importe=prompt("Ingrese el importe del dia: "+contador);
+		importe=parseInt(importe);
+		acumulador=acumulador+importe;
+
+
+		if (contador == 1 )
+
+		{
+			max= importe;
+			min= importe;
+		 }
+
+		 if (max < importe)
+		{
+			
+			max= importe;
+		}
+
+		else if (min > importe)
+		{
+			
+			min= importe;
+		}
+
+
+		if (importe == 0  || importe == null)
+
+		{
+			importe= prompt("Ingrese el importe nuevamente del dia: "+contador);
+		}
+
+		
+
+	}
+		
+		document.getElementById('importeFinal').value= acumulador;
+		alert("El numero máximo fue: "+max);
+		alert("El numero mínimo fue: "+min);
+
+
 
 }
--- 9-Parciales/parcial/siete.js
+++ 9-Parciales/parcial/siete.js
@@ -1,4 +1,91 @@
 function Mostrar()
+
+//Realizar el algoritmo que permita el ingreso por prompt de las notas (validar entre 0 y 10) y el sexo
+//(validar el sexo “f” o “m”) de 100 alumnos, informar por alert:
+//a)el promedio de las notas totales.
+//b)la nota más baja.
+//c)la cantidad de varones que su nota haya sido mayor o igual a 6.
+
 {
+	var notas;
+	var contador= 0;
+	var acumulador= 0;
+	var min;
+	var promedios;
+	var cantivarones= 0;
+	var sexo;
+
+
+	while (contador <= 2)
+
+		{
+			
+
+			contador++;	
+			sexo= prompt("Ingrese su sexo");
+			notas= prompt("Ingrese la nota del alumno "+contador);
+			notas= parseInt(notas);
+			acumulador= acumulador+notas;
+
+			if (notas >= 0 && notas <= 10 )
+			{ 	
+				
+			}
+
+			 else 
+			 {
+			 	notas= prompt("Ingrese nota nuevamente");
+			 }
+			
+
+			if (!(sexo == "f" || sexo== "m" ))
+
+			{
+				sexo= prompt("Ingrese sexo nuevamente");
+			}
+			
+			if (sexo == "f")
+
+			{
+				sexo= "femenino";
+			}
+
+			else (sexo== "m")
+			{
+				sexo== "masculino";
+			}
+
+			if (sexo > 0)
+
+				{			
+					cantivarones+= sexo;
+					cantivarones++;			
+				}
+
+		}
+
+				
+
+				if (contador== 3)
+
+				{
+					min=notas;
+				}
+
+				else if (min > notas)
+
+				{
+					min=notas;
+					
+				}
+				alert("La nota minima es: "+min);
+
+				promedios= acumulador/contador;
+				alert("El promedio es: "+promedios);
+
+				
+				alert("La cantidad de varones es "+cantivarones);
+
+
 
 }
