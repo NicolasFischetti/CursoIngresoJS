98b5fdda790df3a716be77185413e6d5160e0457
NicolasFischetti <36281576+NicolasFischetti@users.noreply.github.com>
2018-03-01 22:22:30 -0300

    Add files via upload

--- 9-Parciales/parcial2/2-entradaSalida.js
+++ 9-Parciales/parcial2/2-entradaSalida.js
@@ -3,5 +3,24 @@ function Mostrar()
 {
 	alert("Funciona 2-EntradaSalida");
 	
+	var importe;
+	var descuento;
+	var resultado;
+
+	descuento= parseInt(descuento);
+	importe= parseInt(importe);
+
+	importe= prompt("Ingrese su importe");
+
+	descuento= (importe * 25) / 100;
+
+	resultado= importe - descuento;
+
+	document.getElementById('importe').value= resultado;
+	
+
+
 }
 
+
+  	
--- 9-Parciales/parcial2/3-entradaSalida.js
+++ 9-Parciales/parcial2/3-entradaSalida.js
@@ -1,7 +1,25 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 3-EntradaSalida");
+	var largo;
+	var ancho;
+	var alambre;
+	var perimetro;
+
+	largo= document.getElementById('largo').value;
+	ancho= document.getElementById("ancho").value;
+
+	largo= parseInt(largo);
+	ancho= parseInt(ancho);
+	alambre= parseInt(alambre);
+
+	perimetro= largo * 2 + ancho * 2;
+
+	alambre= perimetro * 6;
+
+	alert("Los metros de alambre son: "+alambre);
+
+
 	
 }
 
