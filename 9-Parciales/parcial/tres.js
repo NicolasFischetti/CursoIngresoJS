caf4c8671e1935964508eb366d52f853dcbe1f9d
NicolasFischetti <36281576+NicolasFischetti@users.noreply.github.com>
2018-03-10 10:35:37 -0300

    Add files via upload

--- 9-Parciales/parcial/tres.js
+++ 9-Parciales/parcial/tres.js
@@ -1,23 +1,52 @@
 function Mostrar()
 {
 
-	var largo;
-	var ancho;
-	var perímetro;
-	var alambre;
+	//var largo;
+	//var ancho;
+	//var perímetro;
+	//var alambre;
 
 
-	largo= document.getElementById('alrgo').value;
-	ancho= document.getElementById("ancho").value;
+	//largo= document.getElementById('alrgo').value;
+	//ancho= document.getElementById("ancho").value;
 
-	largo= parseInt(largo);
-	ancho= parseInt(ancho);
+	//largo= parseInt(largo);
+	//ancho= parseInt(ancho);
 
-	perimetro= largo + ancho;
+	//perimetro= largo * 2  + ancho * 2;
 
-	alambre= perimetro / 3;
+	//alambre= perimetro * 3;
 
-	alert("Los metros necesarios son: "+alambre)
+	//alert("Los metros necesarios son: "+alambre);
+
+	var precio;
+	var descuento;
+	var preciofinal;
+	var iva;
+	var precioconiva;
+	
+	precio= document.getElementById('alrgo').value;
+	descuento= document.getElementById('ancho').value;
+
+	precio= parseInt(precio);
+	descuento= parseInt(descuento);
+	
+
+	preciofinal= (precio * descuento) / 100;
+	preciocondescuento= precio - preciofinal;
+	iva= (preciocondescuento * 21) / 100;
+	precioconiva= preciocondescuento + iva;
+
+
+	alert("El precio final es "+preciocondescuento);
+	alert("El iva sobre el precio con descuento es "+precioconiva);
+
+
+
+
+
+
+	
 
 
 }
