3ece4550ebd399ea6521ec30d78c9d7f964ffc33
NicolasFischetti <36281576+NicolasFischetti@users.noreply.github.com>
2018-03-10 11:37:56 -0300

    Add files via upload

--- uno.js
+++ uno.js
@@ -1,13 +1,5 @@
 
 function Mostrar()
 {
-	var cuadrado;
-	var lado;
 
-	lado= document.getElementById('laBase').value;
-	var resultado= parseInt(lado);
-
-	var perimetro= lado * 4;
-
-	alert("El perimetro es: "+perimetro);
 }
